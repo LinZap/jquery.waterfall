a9c626f443fe7f19e0f06921744fef72b7fe49f2
Zap <soldierzx0705@gmail.com>
2015-04-24 18:29:47 +0800

    hot fix
    1. fix mutli-container exist, no effect
    2. add container hash id

--- waterfall-light.js
+++ waterfall-light.js
@@ -8,7 +8,6 @@
 
 (function($){
 
-
 	// waterfall option of every elements
 	var g_option = {
 		// default setting 
@@ -22,32 +21,36 @@
 			timer : false,
 			scrollbottom : false
 		}
-	}
+	},
+	// container id
+	hash = 0;
 
 	// waterfall methods (can be called)
 	var methods = {
 		init : function() {
 			// first call, set default
-			if(!g_option[this.selector])g_option[this.selector]=g_option._init_;
+
+			var id = getHashId(this);
+
+			if(!g_option[id])g_option[id]=$.extend({},g_option._init_);
 
 			// overwrite setting
-			if(arguments[0])g_option[this.selector]=$.extend(g_option[this.selector],arguments[0]);
+			if(arguments[0])g_option[id]=$.extend(g_option[id],arguments[0]);
 	
 			// if set scroll to bottom, overwrite setting`
-			if(g_option[this.selector].scrollbottom){
-				g_option[this.selector].scrollbottom = $.extend({
+			if(g_option[id].scrollbottom){
+				g_option[id].scrollbottom = $.extend({
 					ele : this.parent(),
 					endele: $('<div>').css({width:'100%',textAlign:'center',position:'absolute'}),
 					endtxt: 'No More Data',
 					gap: 300
-				},g_option[this.selector].scrollbottom);
+				},g_option[id].scrollbottom);
 			}
 
 			// Waterfall is a absolute-position base layout
 			// the container must be relative or absolute position
 			// This setting needs to be strengthened
 			this.css('position', 'relative');
-			//console.log(g_option[this.selector]);
 			// start
 			detect(this);
 	  	},
@@ -55,31 +58,31 @@
 		sort : function() { 
 			sorting(this); 
 		},
-		// restart : function() { 
-		// 	detect(); 
-		// },
 		stop : function() {
-			if(g_option[this.selector].timer){ 
-				clearInterval(g_option[this.selector].timer); 
-				g_option[this.selector].timer = false; 
+			var id = getHashId(this);
+			if(g_option[id].timer){ 
+				clearInterval(g_option[id].timer); 
+				g_option[id].timer = false; 
 			}
 		},
 		end : function() {
-
-			if(g_option[this.selector].scrollbottom){
-				g_option[this.selector].scrollbottom.ele.css('top',g_option[this.selector].top[getMaxCol(this)]+"px");
-				this.append(g_option[this.selector].scrollbottom.ele);
+			var id = getHashId(this);
+			if(g_option[id].scrollbottom){
+				g_option[id].scrollbottom.ele.css('top',g_option[id].top[getMaxCol(this)]+"px");
+				this.append(g_option[id].scrollbottom.ele);
 			}
-			// if(g_option[this.selector].timer){ 
-			// 	clearInterval(g_option[this.selector].timer); 
-			// 	g_option[this.selector].timer = false; 
-			// }		
+			if(g_option[id].timer){ 
+				clearInterval(g_option[id].timer); 
+				g_option[id].timer = false; 
+			}		
+
 		}
 	};
 
 
 	function getMinCol(t){
-		var top = g_option[t.selector].top,
+		var id = getHashId(t);
+		var top = g_option[id].top,
 			col = 0,
 			min =top[col];
 		for(var i=0;i<top.length;i++) 
@@ -88,62 +91,61 @@
 	}
 
 	function getMaxCol(t){
-		var top = g_option[t.selector].top,
+		var id = getHashId(t);
+		var top = g_option[id].top,
 			col = 0,
 			max = top[col];
 		for(var i=0;i<top.length;i++) 
 			if(top[i]>max){ max = top[i]; col = i;}
 		return col;
 	}
-
 	function sorting(t){
-
-		var gw = g_option[t.selector].gridWidth,
-			w = g_option[t.selector].w,
-			gap =g_option[t.selector].gap,
-			scrollbottom = g_option[t.selector].scrollbottom;
+		var id = getHashId(t);
+		var gw = g_option[id].gridWidth,
+			w = g_option[id].w,
+			gap =g_option[id].gap,
+			scrollbottom = g_option[id].scrollbottom;
 		
-		g_option[t.selector].col = 1;
-		g_option[t.selector].top=[];
+		g_option[id].col = 1;
+		g_option[id].top=[];
 
 		for(var i=gw.length-1;i>=0;i--)
-			if(w>gw[i]){ g_option[t.selector].col = i+1; break; }
+			if(w>gw[i]){ g_option[id].col = i+1; break; }
 
-		var cwidth =(w-((g_option[t.selector].col-1)*gap))/g_option[t.selector].col,
+		var cwidth =(w-((g_option[id].col-1)*gap))/g_option[id].col,
 			left=[];
 
-			//console.log(cwidth);
-		for(var i=0;i<g_option[t.selector].col;i++){
+		for(var i=0;i<g_option[id].col;i++){
 			left.push(i*cwidth+i*gap);
-			g_option[t.selector].top.push(0);
+			g_option[id].top.push(0);
 		}
 		t.children().css({
 			position:'absolute',
 			left: (w/2-cwidth/2)+'px',
 			top: t.scrollTop(),
-			transition: 'left ' + g_option[t.selector].refresh + 'ms ease-in-out,' +
-						'top ' + g_option[t.selector].refresh + 'ms ease-in-out,' +
-						'opacity ' + g_option[t.selector].refresh + 'ms ease-in-out'
+			transition: 'left ' + g_option[id].refresh + 'ms ease-in-out,' +
+						'top ' + g_option[id].refresh + 'ms ease-in-out,' +
+						'opacity ' + g_option[id].refresh + 'ms ease-in-out'
 			}).each(function(i) {
 				var ic = getMinCol(t);
-				$(this).css({ width: cwidth+'px', left: left[ic]+'px', top : g_option[t.selector].top[ic]+'px',opacity:'1' });
-				g_option[t.selector].top[ic]+=$(this)[0].offsetHeight+gap;
+				$(this).css({ width: cwidth+'px', left: left[ic]+'px', top : g_option[id].top[ic]+'px',opacity:'1' });
+				g_option[id].top[ic]+=$(this)[0].offsetHeight+gap;
 			});
 			if(scrollbottom)
-				if(scrollbottom.endele)scrollbottom.endele.css({top:g_option[t.selector].top[getMaxCol(t)]+"px"});
+				if(scrollbottom.endele)scrollbottom.endele.css({top:g_option[id].top[getMaxCol(t)]+"px"});
 	}
 
 	// detect screen width change , resort cards
 	function detect(t){
-		//console.log(g_option[this.selector],this);
-		if(!g_option[t.selector].timer){
-			g_option[t.selector].timer = setInterval(function(){
+		var id = getHashId(t);
+		if(!g_option[id].timer){
+			g_option[id].timer =  setInterval(function(){
 				var bw = t[0].offsetWidth;
-				if(g_option[t.selector].w!=bw) {  g_option[t.selector].w=bw;  sorting(t); }
-				if(g_option[t.selector].scrollbottom)
-					if(g_option[t.selector].scrollbottom.callback && isbottom(g_option[t.selector].scrollbottom.ele,g_option[this.selector].scrollbottom.gap))
-						g_option[t.selector].scrollbottom.callback(t);
-			},g_option[t.selector].refresh);
+				if(g_option[id].w!=bw) {  g_option[id].w=bw;  sorting(t); }
+				if(g_option[id].scrollbottom)
+					if(g_option[id].scrollbottom.callback && isbottom(g_option[id].scrollbottom.ele,g_option[id].scrollbottom.gap))
+						g_option[id].scrollbottom.callback(t);
+			},g_option[id].refresh);
 		}
 		sorting(t);
 	}
@@ -154,6 +156,15 @@
 		return ((ele.innerHeight()+ele.scrollTop())>(ele.prop("scrollHeight")-gap));
 	}
 
+
+	// get element unique id
+	function getHashId(t){
+		if(!t.attr('wf-id')){
+			hash+=0.1;
+			t.attr('wf-id',hash);
+		}
+		return t.attr('wf-id');
+	}
 
 	$.fn.waterfall = function() {
 		var res;
